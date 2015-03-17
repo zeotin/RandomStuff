2421ee8fcd97837e7ea4dc1daff676afc1b241ea
Zeo <dotnet.developer@outlook.com>
2015-03-16 21:55:41 -0400

    init

---
+++ GitTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("asdfasdfasd");
+            Console.WriteLine("What if computers spoke english, and humans speaked in binary ");
+            Console.ReadLine();
+            
+            
+        }
+    }
+}
